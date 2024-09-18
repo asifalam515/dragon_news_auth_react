fb40a3e6574abe52386621510cdc7acc62e7d748
asifalam515 <asibulalam515@gmail.com>
2024-09-18 09:19:08 +0600

    layout created

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+
 const LeftSideNav = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("data/categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
   return (
-    <div>
-      <h2 className="text-2xl">Left side nav</h2>
+    <div className="space-y-6">
+      <h2 className="text-2xl">All Categories:</h2>
+      {categories.map((category) => (
+        <NavLink
+          to={`/category/${category.id}`}
+          className="block ml-4 text-xl font-semibold "
+          key={category.id}
+        >
+          {category.name}
+        </NavLink>
+      ))}
     </div>
   );
 };
