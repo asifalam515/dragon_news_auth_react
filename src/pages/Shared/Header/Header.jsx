79027757ab98e7ba259f3cf5c865439ebf21af73
asifalam515 <asibulalam515@gmail.com>
2024-09-18 07:01:22 +0600

    marquee added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -2,8 +2,8 @@ import moment from "moment";
 import logo from "../../../assets/logo.png";
 const Header = () => {
   return (
-    <div>
-      <img src={logo} alt="" />
+    <div className="text-center">
+      <img className="mx-auto" src={logo} alt="" />
       <p>Journalism Without Fear or Favour</p>
       <p className="text-xl"> {moment().format("Do MMMM D, YYYY, h:mm a")}</p>
     </div>
